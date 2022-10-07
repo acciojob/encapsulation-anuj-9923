7c8a448b6165e4c285890838b149cda640c2f4db
Anuj Kumar <anujkumar0799@gmail.com>
2022-10-07 18:49:27 +0530

    Implemented encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly ob=new RWOnly();
+        ob.setName("Anuj Kumar");
+        System.out.println(ob.getName());
+    }
 }
